8ff73f1b3930a30d5420034cff58e19aaf34789d
MrGabo06 <ggabriel0635@gmail.com>
2026-04-16 05:06:10 -0600

    [ADD]: reconstruccion de señal

--- arduino/lcd/lcd.ino
+++ arduino/lcd/lcd.ino
@@ -1,15 +1,17 @@
 #include <SPI.h>
 #include <LiquidCrystal.h>
+#include <math.h>
 
 LiquidCrystal lcd(2,3,4,5,6,7);
 
 #define BINS 16
+#define PWM_PIN 9
 
 volatile byte data;
 volatile bool flag = false;
 
 byte spectrum[BINS];
-byte index = 0;
+byte idx = 0;
 
 byte bars[8][8] = {
   {0,0,0,0,0,0,0,0},
@@ -22,13 +24,17 @@ byte bars[8][8] = {
   {0,31,31,31,31,31,31,31}
 };
 
+float phase1 = 0;
+float phase2 = 0;
+
 void setup() {
   lcd.begin(16,2);
-  for (int i = 0; i < 8; i++) {
-    lcd.createChar(i, bars[i]);
-  }
+  for (int i = 0; i < 8; i++) lcd.createChar(i, bars[i]);
+
   pinMode(MISO, OUTPUT);
   pinMode(10, INPUT_PULLUP);
+  pinMode(PWM_PIN, OUTPUT);
+
   SPCR = _BV(SPE);
   SPI.attachInterrupt();
 }
@@ -39,15 +45,28 @@ ISR(SPI_STC_vect) {
 }
 
 void loop() {
-  if (flag) {
-    spectrum[index++] = data;
 
-    if (index >= BINS) {
-      index = 0;
+  if (flag) {
+    spectrum[idx++] = data;
+
+    if (idx >= BINS) {
+      idx = 0;
 
       byte maxVal = 1;
+      int maxIndex = 0;
+      int secondIndex = 1;
+
       for (int i = 0; i < 16; i++) {
-        if (spectrum[i] > maxVal) maxVal = spectrum[i];
+        if (spectrum[i] > maxVal) {
+          maxVal = spectrum[i];
+          maxIndex = i;
+        }
+      }
+
+      for (int i = 0; i < 16; i++) {
+        if (i != maxIndex && spectrum[i] > spectrum[secondIndex]) {
+          secondIndex = i;
+        }
       }
 
       for (int i = 0; i < 16; i++) {
@@ -58,9 +77,28 @@ void loop() {
 
         lcd.setCursor(i, 1);
         lcd.write(bottom);
-
         lcd.setCursor(i, 0);
         lcd.write(top);
+      }
+
+      float freq1 = maxIndex * 50.0;
+      float freq2 = secondIndex * 50.0;
+
+      for (int t = 0; t < 100; t++) {
+        float sample =
+          100 * sin(phase1) +
+          80 * sin(phase2);
+
+        int pwm = (int)(127 + sample);
+        if (pwm < 0) pwm = 0;
+        if (pwm > 255) pwm = 255;
+
+        analogWrite(PWM_PIN, pwm);
+
+        phase1 += 2 * PI * freq1 / 8000.0;
+        phase2 += 2 * PI * freq2 / 8000.0;
+
+        delayMicroseconds(125);
       }
     }
 
